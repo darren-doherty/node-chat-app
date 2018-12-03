739b8f811e72a487d16d086f173aeaad429b3758
Darren Doherty <darren.doherty@gmail.com>
2018-12-03 20:35:08 +0000

    added initial socket communication between client and server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ require("./config/config");
 const path = require('path');
 const _ = require("lodash");
 const express = require("express");
+const http = require('http');
+const socketIO = require('socket.io');
 const bodyParser = require("body-parser");
 
 const { authenticate } = require("./middleware/authenticate");
@@ -13,6 +15,16 @@ const publicPath = path.join(__dirname, '../public');
 console.log(publicPath);
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+      console.log('Disconnected from client');
+  });
+});
 
 const port = process.env.PORT;
 
@@ -20,7 +32,7 @@ app.use(bodyParser.json());
 app.use(logging);
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`server listening on port ${port}`);
 });
 
